d0fb31f9bb32a2eafa15e41b6427ca7bb00caf7e
spandanaraveendra-tech <spandana.raveendra@gmail.com>
2026-03-29 10:55:21 +0530

    Initial commit - Digital Clock project
    This is a simple Digital Clock web application built using HTML, CSS, and JavaScript. It displays the current time in real-time with a clean and responsive design. The project demonstrates basic JavaScript concepts like Date and time functions.

--- script.js
+++ script.js
@@ -1,81 +1,20 @@
-let input = document.getElementById("input");
+let hrs = document.getElementById("hrs")
+let min = document.getElementById("min")
+let sec = document.getElementById("sec")
+let am = document.getElementsByTagName("span")[1]
 
-document.getElementById("one").addEventListener("click", () => {
-    input.value += "1";
-});
 
-document.getElementById("two").addEventListener("click", () => {
-    input.value += "2";
-});
-
-document.getElementById("three").addEventListener("click", () => {
-    input.value += "3";
-});
-
-document.getElementById("four").addEventListener("click", () => {
-    input.value += "4";
-});
-
-document.getElementById("five").addEventListener("click", () => {
-    input.value += "5";
-});
-
-document.getElementById("six").addEventListener("click", () => {
-    input.value += "6";
-});
-
-document.getElementById("seven").addEventListener("click", () => {
-    input.value += "7";
-});
-
-document.getElementById("eight").addEventListener("click", () => {
-    input.value += "8";
-});
-
-document.getElementById("nine").addEventListener("click", () => {
-    input.value += "9";
-});
-
-document.getElementById("z").addEventListener("click", () => {
-    input.value += "0";
-});
-
-document.getElementById("zs").addEventListener("click", () => {
-    input.value += "00";
-});
-
-document.getElementById("add").addEventListener("click", () => {
-    input.value += "+";
-});
-
-document.getElementById("sub").addEventListener("click", () => {
-    input.value += "-";
-});
-
-document.getElementById("mul").addEventListener("click", () => {
-    input.value += "*";
-});
-
-document.getElementById("div").addEventListener("click", () => {
-    input.value += "/";
-});
-
-document.getElementById("dot").addEventListener("click", () => {
-    input.value += ".";
-});
-
-document.getElementById("clear").addEventListener("click", () => {
-    input.value = "";
-});
-
-document.getElementById("del").addEventListener("click", () => {
-    input.value = input.value.slice(0, -1);
-});
-
-document.getElementById("eq").addEventListener("click", () => {
-    try {
-        input.value = eval(input.value);
-    } catch {
-        input.value = "Error";
+setInterval(()=>{
+    let date = new Date();
+    let h = date.getHours()
+    let m = date.getMinutes()
+    let s = date.getSeconds()
+    if(h>12){
+        h = h % 12;
+        hrs.innerText = h;
+        am.innerText = "pm";
     }
-});
+    hrs.innerText = h
+    min.innerText = m
+    sec.innerText = s
+},1000)
